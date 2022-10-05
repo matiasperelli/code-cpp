bd8a355f394105b36dcc11db443be4eb18345848
mperelli <101881175+matiasperelli@users.noreply.github.com>
2022-10-04 22:50:30 -0300

    Update código proyecto.cpp

--- código proyecto.cpp
+++ código proyecto.cpp
@@ -1,41 +1,64 @@
 #include <iostream>
-#include <conio.h>
+
 using namespace std;
 
-struct complejo{
-    float real, imaginario;
-}z1,z2;
-
-void datos(); //solicitar datos
-complejo suma(complejo,complejo);
-void resultado(complejo);
-
-int main(){
-    datos();
-    complejo x = suma(z1,z2); //resultado de la suma de ambas partes
-    resultado(x);
-    getch();
+int main() {
+    cout <<"Calculadora de numeros complejos" << endl;
+    cout <<"\nIngrese datos del primer numero complejo: " << endl;
     
+    int real1{};
+    cout <<"Parte real: "; cin >> real1;
+    int imaginaria1{};
+    cout <<"Parte imaginaria: "; cin >> imaginaria1;
+    
+    cout <<"\t\nIngrese datos del segundo numero complejo: " << endl;
+    int real2{};
+    cout <<"Parte real: "; cin >> real2;
+    int imaginaria2{};
+    cout <<"Parte imaginaria: "; cin >> imaginaria2;
+    
+    int opcion{};
+    
+    cout <<"\n1 Suma, 2 Resta, 3 Multiplicacion "; cin >> opcion;
+    
+    int real_resultado{};
+    int imaginario_resultado{};
+    
+    if ( opcion == 1 ) {
+    real_resultado = real1 + real2;
+    imaginario_resultado = imaginaria1 + imaginaria2;
+  } else if ( opcion == 2 ) {
+    real_resultado = real1 - real2;
+    imaginario_resultado = imaginaria1 - imaginaria2;
+  } else if ( opcion == 3 ) {
+    real_resultado = real1*real2 - imaginaria1*imaginaria2;
+    imaginario_resultado = imaginaria1*real2 + real1*imaginaria2;
+  } else {
     return 0;
+  }
+
+  if ( imaginario_resultado == 0 ) {
+    std::cout << real_resultado << std::endl;
+  } else if ( real_resultado == 0 ) {
+    if ( imaginario_resultado == 1 ) {
+      std::cout << "i" << std::endl;
+    } else if ( imaginario_resultado == -1 ) {
+      std::cout << "-i" << std::endl;
+    } else {
+      std::cout << imaginario_resultado << "i" << std::endl;
     }
+  } else {
 
-void datos(){
-    cout <<"Ingrese datos del primer numero complejo: " << endl;
-    cout <<"parte real: "; cin >> z1.real;
-    cout <<"Parte imaginaria: "; cin >> z1.imaginario;
-    
-    cout <<"\nIngrese datos del segundo numero complejo: " << endl;
-    cout <<"parte real: "; cin >> z2.real;
-    cout <<"Parte imaginaria: "; cin >> z2.imaginario;
-}
+    if ( imaginario_resultado <= -2 ) {
+      std::cout << real_resultado << " - " << (-imaginario_resultado) << "i" << std::endl;
+    } else if ( imaginario_resultado == -1 ) {
+      std::cout << real_resultado << " - i" << std::endl;
+    } else if ( imaginario_resultado == 1 ) {
+      std::cout << real_resultado << " + i" << std::endl;
+    }  else {
+      std::cout << real_resultado << " + " << imaginario_resultado << "i" << std::endl;
+    }
+  }
 
-complejo suma(complejo z1,complejo z2){
-    z1.real += z2.real;
-    z1.imaginario += z2.imaginario;
-    
-    return z1;
-}
-
-void resultado(complejo x){
-    cout <<"\nLa suma es: " << x.real << "+" << x.imaginario << "i" << endl;
+  return 0;
 }
