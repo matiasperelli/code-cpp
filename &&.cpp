9ee444e8079f85fdac6c874fa95acc7b73c06313
mperelli <101881175+matiasperelli@users.noreply.github.com>
2022-10-17 19:54:51 -0300

    Create &&.cpp

---
+++ &&.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int edad;
+    
+    cout <<"Digite su edad: "; cin >> edad;
+    
+    if((edad>=18)&&(edad<=25)){ //operador "y" logico, se deben cumplir ambas 
+        cout <<"Su edad esta en el rango [18-25]"
+    }
+    else{
+        cout << "Su edad no se encuentra en dicho rango"
+    }
+    return 0;
+}
