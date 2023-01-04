56a7a72713de9cd3fa3bd764dee7f205c9720ce5
mperelli <101881175+matiasperelli@users.noreply.github.com>
2023-01-04 20:25:51 -0300

    Update factorial con for.cpp

--- factorial con for.cpp
+++ factorial con for.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-    int n, suma = 1;
+    int n, factorial = 1;
     
     cout <<"Digite un numero: "; cin >> n;
     
     for(int i = 1; i <= n; i++){
-        suma *= i;
+        factorial *= i;
     }
     
-    cout <<"Su factorial es: " << suma << endl;
+    cout <<"Su factorial es: " << factorial << endl;
     
     return 0;
 }
